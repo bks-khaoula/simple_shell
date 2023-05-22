58a95be79f891931a1430c4130873062ce5a3821
khaoula <bakasskhaoula9@gmail.com>
2023-05-22 05:40:06 -0700

    A code that passes the Betty checks

---
+++ betty_check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * description - example code that passes the Betty checks
+ */
+void main(void)
+{
+	int variable = 0;
+
+	printf("%d\n", variable);
+}
