fcc424bca7473e6991214d36d3202337210838e8
khaoula <bakasskhaoula9@gmail.com>
2023-05-26 12:14:38 -0700

    beautiful code that passes the Betty checks

---
+++ betty_checker.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this a the main function
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int var = 2;
+
+	if (var == 2)
+		printf("Statement is True: %d\n", var);
+	else
+		printf("Statement is False!\n");
+
+	printf("I passed Betty test \n");
+
+	return (0);
+}
